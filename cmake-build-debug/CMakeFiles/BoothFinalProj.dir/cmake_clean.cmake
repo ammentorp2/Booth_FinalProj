40a8a8ae8ed84ca4a088394b01365132fe864b82
Andrew Ammentorp <andrew_ammentorp1@baylor.edu>
2020-04-18 16:11:38 -0500

    Initial Project

---
+++ cmake-build-debug/CMakeFiles/BoothFinalProj.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BoothFinalProj.exe"
+  "BoothFinalProj.exe.manifest"
+  "BoothFinalProj.pdb"
+  "CMakeFiles/BoothFinalProj.dir/main.cpp.obj"
+  "libBoothFinalProj.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BoothFinalProj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
